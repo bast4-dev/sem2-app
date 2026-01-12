50cbadaf0e6bb6971f3d8e9976aa218df8a2da3f
Bastien <bastien-stachs@hotmail.fr>
2026-01-12 11:26:21 +0100

    1st commit

---
+++ app/Models/Settings.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Settings extends Model
+{
+    protected $fillable = ['theme', 'lang'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
